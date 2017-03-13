f08a518a7851383c77be46e189188d96b8047955
Eduardo Testón Oliva <Eduardo Testn Oliva>
2017-03-14 00:08:25 +0100

    Finish task 11

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,64 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
-                <!-- Exercise -->
-                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-                <p>{{ testClick }} </p>
+                <h1>Filters & Mixins</h1>
 
-                <button class="btn btn-primary"
-                    v-myon:click="eventClick" 
-                    >Owner click</button>
-                <button class="btn btn-info"
-                    @click="testClick += ' >>OFFICIAL<< '" 
-                    >Vuejs click</button>
+                <!-- Exercise 1) -->
+                <!-- Build a local Filter which reverses the Text it is applied on -->
+                <h3>Exercise 1</h3>
+                <input v-model="textToReverse">
+                <p>{{ textToReverse | toReverse }}</p>
+                <hr>
+                <!-- Exercise 2 -->
+                <!-- Build a global Filter which counts the length of a word and it appends it -->
+                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+                <h3>Exercise 2</h3>
+                <input v-model="textToCountWords">
+                <p>{{ textToCountWords | count-words }}</p>
+                <hr>
+
+                <!-- Exercise 3 -->
+                <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
+                <h3>Exercise 3</h3>
+                <h4>Text showed from computed</h4>
+                <p>{{ textToReverseComputed }}</p>
+                
+                <h4>Count showed from computed</h4>
+                <p>{{countWordsComputed }}</p>
+                <hr>
+
+                <!-- Exercise 4 -->
+                <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
+                <h3>Exercise 4</h3>                
+                <input v-model="textToCountWordsMixin">
+                <p>{{ countWordsComputedMixin }}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import { CountWordsMixin } from './CountWordsMixin'
+
     export default {
+        mixins: [CountWordsMixin],
         data () {
             return {
-                testClick: 'Click the buttons'
+                textToReverse: 'Example text to reverse',
+                textToCountWords: 'Text to count the words'
             }
         },
-        methods: {
-            eventClickERROR: function(expression) {
-                this.testClick += ' ' + expression
+        filters: {
+            toReverse(value) {
+                return value.split("").reverse().join("");
+            }
+        },
+        computed: {
+            textToReverseComputed() {
+                return this.textToReverse.split("").reverse().join("");
             },
-            eventClick: function() {
-                this.testClick += ' ' + 'test'
-            }
-        },
-        directives: {
-            'myon' : {
-                bind(el, binding, vnode) {
-                    // if (binding.arg == 'click') {
-                    //     el.click(binding.expression);
-                    //     console.info('[BIND]Attach click ' + JSON.stringify(el));
-                    //     console.info('[BIND]Expression: ' + binding.expression);
-                    // }
-
-                    el.addEventListener(binding.arg, binding.value);
-                }
+            countWordsComputed() {
+                return this.textToCountWords.split(" ").length;
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('to-lowercase', function(value) {
+    return value.toLowerCase();
+});
+
+Vue.filter('count-words', function(value) {
+    return value.split(" ").length;
+});
+
+Vue.mixin({
+    created() {
+        console.log('Global Mixin - Created Hook');
+    }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
